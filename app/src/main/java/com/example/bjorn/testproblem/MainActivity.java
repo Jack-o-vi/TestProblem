ec651ed65cc58f1b48bc11fc421b1a2f5bcf5ceb
Vitaly Zeienko <jackovi123@gmail.com>
2018-05-31 17:11:01 +0300

    Task is done, now we can check if intern can perform 12 tasks in specified time.

--- app/src/main/java/com/example/bjorn/testproblem/MainActivity.java
+++ app/src/main/java/com/example/bjorn/testproblem/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.bjorn.testproblem;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "[MainActivity]";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+    }
+
+    public String perform(String tasks, String hours) {
+        int minF = 1;
+        int maxF = 11;
+        int minH = 1;
+        int maxH = 24;
+        int iterTime = 55;
+        int minInH = 60;
+        try {
+            int f = Integer.parseInt(tasks);
+            int h = Integer.parseInt(hours);
+
+            if (!(minF <= f && f <= maxF && minH <= h && h <= maxH)) return "Invalid number";
+
+            int remain = Testing.getTasksMax() - f;
+            int result = 0;
+            for (int i = 0; i < remain - 1; i++) {
+                result += iterTime;
+            }
+            Log.d(TAG, "Result min: " + result + " Min: " + ((h - 1) * minInH));
+            if (result <= ((h - 1) * minInH))
+                return "Yes";
+            else return "No";
+
+
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return "Wrong number format";
+        }
+
+
+    }
+
+    public void onclick(View v) {
+        EditText etTasks = findViewById(R.id.etTasks);
+        EditText etHours = findViewById(R.id.etHours);
+        TextView tvResult = findViewById(R.id.tvResult);
+        String result = perform(etTasks.getText().toString(), etHours.getText().toString());
+        if(result != null){
+            tvResult.setText(result);
+        }
     }
 }
--- app/src/main/java/com/example/bjorn/testproblem/Testing.java
+++ app/src/main/java/com/example/bjorn/testproblem/Testing.java
@@ -1,7 +1,5 @@
 package com.example.bjorn.testproblem;
 
-import java.util.Scanner;
-
 public class Testing {
 
     public static final int TASKS_MAX = 12;
@@ -16,54 +14,4 @@ public class Testing {
     public static int getTasksMax() {
         return TASKS_MAX;
     }
-
-    public static void main(String[] args) {
-
-
-        int minF = 1;
-        int maxF = 11;
-        int minH = 1;
-        int maxH = 24;
-        int iterTime = 55;
-        int minInH = 60;
-        try (Scanner sc = new Scanner(System.in)) {
-            int f;
-            int h;
-            do {
-                f = sc.nextInt();
-                h = sc.nextInt();
-            } while (!(minF <= f && f <= maxF && minH <= h && h <= maxH));
-
-            Testing t = new Testing(f, h);
-
-            int remain = Testing.getTasksMax() - f;
-            int result = iterTime;
-            for (int i = 0; i < remain-1; i++) {
-                result += result;
-            }
-
-            if (result <= ((h - 1) * 60))
-                System.out.println("Yes");
-            else System.out.println("No");
-
-        }
-
-    }
-
-    public int getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(int tasks) {
-        this.tasks = tasks;
-    }
-
-    public int getHours() {
-        return hours;
-    }
-
-    public void setHours(int hours) {
-        this.hours = hours;
-    }
-
 }
